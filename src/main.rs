40e134d00a85d47a4f3d5b69f019c3d690ec482d
adtak <adtak62@gmail.com>
2021-06-04 23:38:55 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate csv;
+
+use std::error::Error;
+use std::io;
+use std::process;
+
+fn main() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record)
+    }
+    Ok(())
+}
